aa34b1ddb2407f02b77395eefdf03effefc09942
JustinMartin7x <justin.martin7x@gmail.com>
2021-02-03 13:25:39 -0800

    recieving data from  api

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchNews, fetchAll } from '../utils/utils';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+
+  const [articles, setArticles] = useState([]);
+
+
+
+  useEffect(() => {
+    let mounted = true;
+    fetchAll().then((items) => {
+      if(mounted) {
+        setArticles(items.articles);
+      }
+    });
+    return () => (mounted = false);
+  }, []);
+
+  console.log('THIS IS A CONSOLE LOG', articles);
+
+  return (
+    <>
+      <h1>Hello Everyone</h1>
+    </>
+  );
 }
